54cbe496c62f9da355f5f3cc387f68102660efce
Gurinder Singh <gurinder.singh@taxmanntechnologies.com>
2024-03-04 18:06:16 +0530

    added hash

--- src/index.js
+++ src/index.js
@@ -5,25 +5,22 @@ import reportWebVitals from './reportWebVitals';
 import * as ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
+  HashRouter,
+  Route,
   RouterProvider,
+  Routes,
 } from "react-router-dom";
 import SubSectionList from './subSectionList';
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    children:[
-     
-    ]
-  },{
-    path: "/sublist/:id",
-    element:<SubSectionList/>,
-  }
-]); 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/sublist/:id" element={<SubSectionList />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
